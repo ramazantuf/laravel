4da76c334323d9075771b62285ba09bc83c4ae17
ramazantufekci <4970844+ramazantufekci@users.noreply.github.com>
2025-08-12 12:55:57 +0300

    register login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,19 +12,18 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Scripts
-        vite(['resources/css/app.css', 'resources/js/app.js']) -->
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-
+    <!-- Scripts -->
+    <!--vite(['resources/css/app.css', 'resources/js/app.js'])-->
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div class="min-h-screen bg-gray-100">
         @include('layouts.navigation')
 
         <!-- Page Heading -->
         @if (isset($header))
-        <header class="bg-white dark:bg-gray-800 shadow">
+        <header class="bg-white shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 {{ $header }}
             </div>
